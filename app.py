17948c4871525c786ca02f442943f8d93e6d210c
vince415 <vincentchu415@gmail.com>
2025-11-21 06:35:28 +0000

    1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
     q = request.form.get("q")
     print(q)
